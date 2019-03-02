fda98722cc1ed1a47aa4e0d5db4d62345fc290bf
leikareipa <drilledicecream@gmail.com>
2019-03-03 01:51:52 +0200

    Move hitable.txt.js from js/lut/ into assets/
    It's not really a lookup table, and more of a game asset, so.

--- index.php
+++ index.php
@@ -38,39 +38,39 @@
             <!-- Menu items for props will be generated here.-->
         </div>
 
-        <script src="js/polyfill.js" defer></script>
+        <script src="./js/polyfill.js" defer></script>
 
         <!-- Third-party JavaScript.-->
-        <script src="js/filesaver/FileSaver.min.js" defer></script>
-        <script src="js/jszip/jszip.min.js" defer></script>
+        <script src="./js/filesaver/FileSaver.min.js" defer></script>
+        <script src="./js/jszip/jszip.min.js" defer></script>
 
-        <!-- RallySportED JavaScript.-->
-        <script src="js/rsed/lut/hitable.txt.js" defer></script>
-        <script src="js/rsed/common.js" defer></script>
-        <script src="js/rsed/visual/color.js" defer></script>
-        <script src="js/rsed/visual/texture.js" defer></script>
-        <script src="js/rsed/visual/palette.js" defer></script>
-        <script src="js/rsed/transform/geometry.js" defer></script>
-        <script src="js/rsed/transform/matrix44.js" defer></script>
-        <script src="js/rsed/transform/poly-transform.js" defer></script>
-        <script src="js/rsed/render/camera.js" defer></script>
-        <script src="js/rsed/render/rsed-renderer.js" defer></script>
-        <script src="js/rsed/render/render-surface.js" defer></script>
-        <script src="js/rsed/track/maasto.js" defer></script>
-        <script src="js/rsed/track/palat.js" defer></script>
-        <script src="js/rsed/track/props.js" defer></script>
-        <script src="js/rsed/track/manifesto.js" defer></script>
-        <script src="js/rsed/ui/font.js" defer></script>
-        <script src="js/rsed/ui/mouse-cursor.js" defer></script>
-        <script src="js/rsed/ui/view.js" defer></script>
-        <script src="js/rsed/ui/brush.js" defer></script>
-        <script src="js/rsed/ui/draw.js" defer></script>
-        <script src="js/rsed/ui/input.js" defer></script>
-        <script src="js/rsed/render/line-draw.js" defer></script>
-        <script src="js/rsed/render/ngon-fill.js" defer></script>
-        <script src="js/rsed/resource-loader.js" defer></script>
-        <script src="js/rsed/project.js" defer></script>
-        <script src="js/rsed/rsed.js" defer></script>
-        <script src="js/rsed/page.js" defer></script>
+        <!-- RallySportED's JavaScript.-->
+        <script src="./assets/hitable.txt.js" defer></script>
+        <script src="./js/rsed/common.js" defer></script>
+        <script src="./js/rsed/visual/color.js" defer></script>
+        <script src="./js/rsed/visual/texture.js" defer></script>
+        <script src="./js/rsed/visual/palette.js" defer></script>
+        <script src="./js/rsed/transform/geometry.js" defer></script>
+        <script src="./js/rsed/transform/matrix44.js" defer></script>
+        <script src="./js/rsed/transform/poly-transform.js" defer></script>
+        <script src="./js/rsed/render/camera.js" defer></script>
+        <script src="./js/rsed/render/rsed-renderer.js" defer></script>
+        <script src="./js/rsed/render/render-surface.js" defer></script>
+        <script src="./js/rsed/track/maasto.js" defer></script>
+        <script src="./js/rsed/track/palat.js" defer></script>
+        <script src="./js/rsed/track/props.js" defer></script>
+        <script src="./js/rsed/track/manifesto.js" defer></script>
+        <script src="./js/rsed/ui/font.js" defer></script>
+        <script src="./js/rsed/ui/mouse-cursor.js" defer></script>
+        <script src="./js/rsed/ui/view.js" defer></script>
+        <script src="./js/rsed/ui/brush.js" defer></script>
+        <script src="./js/rsed/ui/draw.js" defer></script>
+        <script src="./js/rsed/ui/input.js" defer></script>
+        <script src="./js/rsed/render/line-draw.js" defer></script>
+        <script src="./js/rsed/render/ngon-fill.js" defer></script>
+        <script src="./js/rsed/resource-loader.js" defer></script>
+        <script src="./js/rsed/project.js" defer></script>
+        <script src="./js/rsed/rsed.js" defer></script>
+        <script src="./js/rsed/page.js" defer></script>
     </body>
 </html>
