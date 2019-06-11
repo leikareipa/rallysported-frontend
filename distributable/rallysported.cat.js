leikareipa/rallysported-frontend

commit 8e4fd6d970e30c9e07bf2b0f66451c7413ea471b

Pre-compute PALA and prop textures
About 40% performance increase; although still running slower than before
the rewrite. The top-down paint view, in particular, is quite slow to
render, at the moment.

distributable/rallysported.cat.js
@@ -1195,10 +1195,15 @@ Rsed.texture = function(args = {})
         default: Rsed.throw("Unknown texture-flipping mode."); break;
     }
 
-    const publicInterface =
+    const publicInterface = Object.freeze(
     {
-        ...args
-    };
+        width: args.width,
+        height: args.height,
+        alpha: args.alpha,
+        flipped: args.flipped,
+        pixels: Object.freeze([].map.call(args.pixels, e=>e)), // Convert into Array, then freeze.
+        indices: Object.freeze([].map.call(args.indices, e=>e)),
+    });
 
     return publicInterface;
 }
@@ -2498,6 +2503,35 @@ Rsed.track.palat = function(palaWidth = 0, palaHeight = 0, data = Uint8Array)
 
     const palaSize = (palaWidth * palaHeight);
 
+    // Pre-compute the individual PALA textures.
+    const palaTextures = new Array(256).fill().map((pala, idx)=>
+    {
+        const dataIdx = (idx * palaSize);
+
+        // For PALA textures that are missing in the source data, return a dummy texture.
+        if ((dataIdx + palaSize) >= data.byteLength)
+        {
+            return Rsed.texture(
+            {
+                width: 1,
+                height: 1,
+                alpha: false,
+                pixels: [Rsed.palette.color("gray")],
+                indices: [0],
+            });
+        }
+
+        return Rsed.texture(
+        {
+            width: palaWidth,
+            height: palaHeight,
+            alpha: false,
+            flipped: "vertical",
+            pixels: pixels.slice(dataIdx, (dataIdx + palaSize)),
+            indices: data.slice(dataIdx, (dataIdx + palaSize)),
+        });
+    });
+
     const publicInterface =
     {
         width: palaWidth,
@@ -2506,33 +2540,11 @@ Rsed.track.palat = function(palaWidth = 0, palaHeight = 0, data = Uint8Array)
         // Returns a copy of the individual PALA texture at the given index.
         texture:(palaId = 0, args = {/*alpha:true|false,*/})=>
         {
-            Rsed.assert && Number.isInteger(palaId)
-                        || Rsed.throw("Expected integer parameters.");
+            Rsed.assert && ((palaId >= 0) &&
+                            (palaId < palaTextures.length))
+                        || Rsed.throw("Attempting to access PALA textures out of bounds.");
 
-            const dataIdx = (palaId * palaSize);
-
-            // If the request is out of bounds, return a dummy texture.
-            if ((dataIdx < 0) || ((dataIdx + palaSize) >= data.byteLength))
-            {
-                return Rsed.texture(
-                {
-                    width: 1,
-                    height: 1,
-                    alpha: args.alpha,
-                    pixels: [Rsed.palette.color("black")],
-                    indices: [0],
-                });
-            }
-
-            return Rsed.texture(
-            {
-                width: palaWidth,
-                height: palaHeight,
-                alpha: args.alpha,
-                flipped: "vertical",
-                pixels: pixels.slice(dataIdx, (dataIdx + palaSize)),
-                indices: data.slice(dataIdx, (dataIdx + palaSize)),
-            });
+            return Object.freeze({...palaTextures[palaId], alpha:args.alpha});
         }
     };
 
@@ -2574,6 +2586,37 @@ Rsed.track.props = async function(textureAtlas = Uint8Array)
 
     const textureRects = data.propTextureRects.filter(m=>(typeof m.textureId !== "undefined"))
                                               .sort((a, b)=>((a.textureId === b.textureId)? 0 : ((a.textureId > b.textureId)? 1 : -1)));
+
+    // Pre-compute the individual prop textures.
+    const propTextures = new Array(textureRects.length).fill().map((tex, idx)=>
+    {
+        const width = textureRects[idx].rect.width;
+        const height = textureRects[idx].rect.height;
+        const pixels = [];
+        const indices = [];
+
+        // Copy the texture's pixel region from the texture atlas.
+        for (let y = 0; y < height; y++)
+        {
+            for (let x = 0; x < width; x++)
+            {
+                const textureAtlasWidth = 128;
+                const dataIdx = ((textureRects[idx].rect.topLeft.x + x) + (textureRects[idx].rect.topLeft.y + y) * textureAtlasWidth);
+
+                indices.push(textureAtlas[dataIdx]);
+                pixels.push(Rsed.palette.color(textureAtlas[dataIdx]));
+            }
+        }
+
+        return Rsed.texture(
+        {
+            width,
+            height,
+            pixels: pixels,
+            indices: indices,
+            flipped: "vertical",
+        });
+    });
 
     const publicInterface =
     {
@@ -2642,48 +2685,13 @@ Rsed.track.props = async function(textureAtlas = Uint8Array)
             }
         },
 
-        // Returns a by-value copy of the given prop texture.
-        texture: (textureId = 0, args = {})=>
+        texture: (textureId = 0, args = {/*alpha: true | false*/})=>
         {
-            args =
-            {
-                ...
-                {
-                    alpha: true,
-                    flipped: "vertical",
-                },
-                ...args
-            };
-            
             Rsed.assert && ((textureId >= 0) &&
-                            (textureId < textureRects.length))
-                        || Rsed.throw("Querying a prop texture out of bounds.");
+                            (textureId < propTextures.length))
+                        || Rsed.throw("Attempting to access prop textures out of bounds.");
 
-            const width = textureRects[textureId].rect.width;
-            const height = textureRects[textureId].rect.height;
-            const pixels = [];
-            const indices = [];
-
-            // Copy the texture's pixel region from the texture atlas.
-            for (let y = 0; y < height; y++)
-            {
-                for (let x = 0; x < width; x++)
-                {
-                    const idx = ((textureRects[textureId].rect.topLeft.x + x) + (textureRects[textureId].rect.topLeft.y + y) * 128);
-
-                    indices.push(textureAtlas[idx]);
-                    pixels.push(Rsed.palette.color(textureAtlas[idx]));
-                }
-            }
-
-            return Rsed.texture(
-            {
-                width,
-                height,
-                pixels: pixels,
-                indices: indices,
-                ...args,
-            });
+            return Object.freeze({...propTextures[textureId], alpha:args.alpha});
         },
 
         name: (propId = 0)=>
@@ -4085,7 +4093,6 @@ Rsed.ui_draw_n = (function()
                     case "3d":
                     case "3d-topdown":
                     {
-                        if (Rsed.core.fps_counter_enabled()) draw_fps();
                         draw_watermark();
                         draw_minimap();
                         draw_active_pala();
@@ -4105,6 +4112,8 @@ Rsed.ui_draw_n = (function()
                     default: break;
                 }
 
+                if (Rsed.core.fps_counter_enabled()) draw_fps();
+                
                 draw_mouse_cursor();
             }
             renderSurface.exposed().putImageData(pixelSurface, 0, 0);
@@ -4938,7 +4947,7 @@ Rsed.ngon_fill_n = (function()
                                         const idx = ((px + py * width) * 4);
                                         
                                         // Solid fill.
-                                        if (texture == null)
+                                        if ((texture == null) || (!texture.pixels))
                                         {
                                             pixelMap.data[idx + 0] = poly.color.r;
                                             pixelMap.data[idx + 1] = poly.color.g;
@@ -5209,9 +5218,6 @@ Rsed.core = (function()
 
         Rsed.palette.set_palette(project.track_id() === 4? 1 :
                                  project.track_id() === 7? 3 : 0);
-
-        /// TODO. Prebake certain project data (like textures) to improve performance.
-        // project.prebake_data();
 
         /// TODO. This needs to be implemented in a better way and/or somewhere
         /// else - ideally so you don't have to manually start the poll loop;

js/rallysported/core.js
@@ -201,9 +201,6 @@ Rsed.core = (function()
         Rsed.palette.set_palette(project.track_id() === 4? 1 :
                                  project.track_id() === 7? 3 : 0);
 
-        /// TODO. Prebake certain project data (like textures) to improve performance.
-        // project.prebake_data();
-
         /// TODO. This needs to be implemented in a better way and/or somewhere
         /// else - ideally so you don't have to manually start the poll loop;
         /// so you don't risk starting it twice or whatever.

js/rallysported/track/props.js
@@ -33,6 +33,37 @@ Rsed.track.props = async function(textureAtlas = Uint8Array)
     const textureRects = data.propTextureRects.filter(m=>(typeof m.textureId !== "undefined"))
                                               .sort((a, b)=>((a.textureId === b.textureId)? 0 : ((a.textureId > b.textureId)? 1 : -1)));
 
+    // Pre-compute the individual prop textures.
+    const propTextures = new Array(textureRects.length).fill().map((tex, idx)=>
+    {
+        const width = textureRects[idx].rect.width;
+        const height = textureRects[idx].rect.height;
+        const pixels = [];
+        const indices = [];
+
+        // Copy the texture's pixel region from the texture atlas.
+        for (let y = 0; y < height; y++)
+        {
+            for (let x = 0; x < width; x++)
+            {
+                const textureAtlasWidth = 128;
+                const dataIdx = ((textureRects[idx].rect.topLeft.x + x) + (textureRects[idx].rect.topLeft.y + y) * textureAtlasWidth);
+
+                indices.push(textureAtlas[dataIdx]);
+                pixels.push(Rsed.palette.color(textureAtlas[dataIdx]));
+            }
+        }
+
+        return Rsed.texture(
+        {
+            width,
+            height,
+            pixels: pixels,
+            indices: indices,
+            flipped: "vertical",
+        });
+    });
+
     const publicInterface =
     {
         // Returns an object containing the given prop's 3d mesh (with properties copied by
@@ -100,48 +131,13 @@ Rsed.track.props = async function(textureAtlas = Uint8Array)
             }
         },
 
-        // Returns a by-value copy of the given prop texture.
-        texture: (textureId = 0, args = {})=>
+        texture: (textureId = 0, args = {/*alpha: true | false*/})=>
         {
-            args =
-            {
-                ...
-                {
-                    alpha: true,
-                    flipped: "vertical",
-                },
-                ...args
-            };
-            
             Rsed.assert && ((textureId >= 0) &&
-                            (textureId < textureRects.length))
-                        || Rsed.throw("Querying a prop texture out of bounds.");
+                            (textureId < propTextures.length))
+                        || Rsed.throw("Attempting to access prop textures out of bounds.");
 
-            const width = textureRects[textureId].rect.width;
-            const height = textureRects[textureId].rect.height;
-            const pixels = [];
-            const indices = [];
-
-            // Copy the texture's pixel region from the texture atlas.
-            for (let y = 0; y < height; y++)
-            {
-                for (let x = 0; x < width; x++)
-                {
-                    const idx = ((textureRects[textureId].rect.topLeft.x + x) + (textureRects[textureId].rect.topLeft.y + y) * 128);
-
-                    indices.push(textureAtlas[idx]);
-                    pixels.push(Rsed.palette.color(textureAtlas[idx]));
-                }
-            }
-
-            return Rsed.texture(
-            {
-                width,
-                height,
-                pixels: pixels,
-                indices: indices,
-                ...args,
-            });
+            return Object.freeze({...propTextures[textureId], alpha:args.alpha});
         },
 
         name: (propId = 0)=>

js/rallysported/render/ngon-fill.js
@@ -144,7 +144,7 @@ Rsed.ngon_fill_n = (function()
                                         const idx = ((px + py * width) * 4);
                                         
                                         // Solid fill.
-                                        if (texture == null)
+                                        if ((texture == null) || (!texture.pixels))
                                         {
                                             pixelMap.data[idx + 0] = poly.color.r;
                                             pixelMap.data[idx + 1] = poly.color.g;

js/rallysported/track/palat.js
@@ -23,6 +23,35 @@ Rsed.track.palat = function(palaWidth = 0, palaHeight = 0, data = Uint8Array)
 
     const palaSize = (palaWidth * palaHeight);
 
+    // Pre-compute the individual PALA textures.
+    const palaTextures = new Array(256).fill().map((pala, idx)=>
+    {
+        const dataIdx = (idx * palaSize);
+
+        // For PALA textures that are missing in the source data, return a dummy texture.
+        if ((dataIdx + palaSize) >= data.byteLength)
+        {
+            return Rsed.texture(
+            {
+                width: 1,
+                height: 1,
+                alpha: false,
+                pixels: [Rsed.palette.color("gray")],
+                indices: [0],
+            });
+        }
+
+        return Rsed.texture(
+        {
+            width: palaWidth,
+            height: palaHeight,
+            alpha: false,
+            flipped: "vertical",
+            pixels: pixels.slice(dataIdx, (dataIdx + palaSize)),
+            indices: data.slice(dataIdx, (dataIdx + palaSize)),
+        });
+    });
+
     const publicInterface =
     {
         width: palaWidth,
@@ -31,33 +60,11 @@ Rsed.track.palat = function(palaWidth = 0, palaHeight = 0, data = Uint8Array)
         // Returns a copy of the individual PALA texture at the given index.
         texture:(palaId = 0, args = {/*alpha:true|false,*/})=>
         {
-            Rsed.assert && Number.isInteger(palaId)
-                        || Rsed.throw("Expected integer parameters.");
+            Rsed.assert && ((palaId >= 0) &&
+                            (palaId < palaTextures.length))
+                        || Rsed.throw("Attempting to access PALA textures out of bounds.");
 
-            const dataIdx = (palaId * palaSize);
-
-            // If the request is out of bounds, return a dummy texture.
-            if ((dataIdx < 0) || ((dataIdx + palaSize) >= data.byteLength))
-            {
-                return Rsed.texture(
-                {
-                    width: 1,
-                    height: 1,
-                    alpha: args.alpha,
-                    pixels: [Rsed.palette.color("black")],
-                    indices: [0],
-                });
-            }
-
-            return Rsed.texture(
-            {
-                width: palaWidth,
-                height: palaHeight,
-                alpha: args.alpha,
-                flipped: "vertical",
-                pixels: pixels.slice(dataIdx, (dataIdx + palaSize)),
-                indices: data.slice(dataIdx, (dataIdx + palaSize)),
-            });
+            return Object.freeze({...palaTextures[palaId], alpha:args.alpha});
         }
     };
 

js/rallysported/ui/draw.js
@@ -353,7 +353,6 @@ Rsed.ui_draw_n = (function()
                     case "3d":
                     case "3d-topdown":
                     {
-                        if (Rsed.core.fps_counter_enabled()) draw_fps();
                         draw_watermark();
                         draw_minimap();
                         draw_active_pala();
@@ -373,6 +372,8 @@ Rsed.ui_draw_n = (function()
                     default: break;
                 }
 
+                if (Rsed.core.fps_counter_enabled()) draw_fps();
+                
                 draw_mouse_cursor();
             }
             renderSurface.exposed().putImageData(pixelSurface, 0, 0);

js/rallysported/visual/texture.js
@@ -51,10 +51,15 @@ Rsed.texture = function(args = {})
         default: Rsed.throw("Unknown texture-flipping mode."); break;
     }
 
-    const publicInterface =
+    const publicInterface = Object.freeze(
     {
-        ...args
-    };
+        width: args.width,
+        height: args.height,
+        alpha: args.alpha,
+        flipped: args.flipped,
+        pixels: Object.freeze([].map.call(args.pixels, e=>e)), // Convert into Array, then freeze.
+        indices: Object.freeze([].map.call(args.indices, e=>e)),
+    });
 
     return publicInterface;
 }

commit 7c8e5fbb0fd3b29150e5aa79e1275b24b20672f2

Fix prop textures not having alpha-testing on by default
Regressed in 8e4fd6d970e30c9e07bf2b0f66451c7413ea471b.

distributable/rallysported.cat.js
@@ -2691,6 +2691,15 @@ Rsed.track.props = async function(textureAtlas = Uint8Array)
                             (textureId < propTextures.length))
                         || Rsed.throw("Attempting to access prop textures out of bounds.");
 
+            args =
+            {
+                ...
+                {
+                    alpha: true,
+                },
+                ...args,
+            };
+
             return Object.freeze({...propTextures[textureId], alpha:args.alpha});
         },
 
@@ -5076,32 +5085,32 @@ Rsed.core = (function()
     {
         const uiContainer = new Vue(
         {
-            el:"#html-ui",
+            el: "#html-ui",
             data:
             {
                 // The display name of the track that's currently open in the editor.
-                trackName:"",
+                trackName: "",
 
-                propList:[],
+                propList: [],
 
                 // Whether the UI should be displayed or kept invisible at this time.
-                uiVisible:false,
+                uiVisible: false,
             },
             methods:
             {
                 // Called when the user selects a prop from the prop dropdown menu.
                 /// TODO: Needs to be somewhere more suitable, and named something more descriptive.
-                activate_prop:function(name = "")
+                activate_prop: function(name = "")
                 {
                     Rsed.core.project().props.change_prop_type(Rsed.core.project().track_id(),
-                                                                 Rsed.ui_input_n.mouse_hover_args().trackId,
-                                                                 Rsed.core.project().props.id_for_name(name));
+                                                               Rsed.ui_input_n.mouse_hover_args().trackId,
+                                                               Rsed.core.project().props.id_for_name(name));
                     window.close_dropdowns();
 
                     return;
                 },
                 
-                refresh:function()
+                refresh: function()
                 {
                     this.trackName = Rsed.core.project().name;
                     this.propList = Rsed.core.project().props.names()

js/rallysported/core.js
@@ -58,32 +58,32 @@ Rsed.core = (function()
     {
         const uiContainer = new Vue(
         {
-            el:"#html-ui",
+            el: "#html-ui",
             data:
             {
                 // The display name of the track that's currently open in the editor.
-                trackName:"",
+                trackName: "",
 
-                propList:[],
+                propList: [],
 
                 // Whether the UI should be displayed or kept invisible at this time.
-                uiVisible:false,
+                uiVisible: false,
             },
             methods:
             {
                 // Called when the user selects a prop from the prop dropdown menu.
                 /// TODO: Needs to be somewhere more suitable, and named something more descriptive.
-                activate_prop:function(name = "")
+                activate_prop: function(name = "")
                 {
                     Rsed.core.project().props.change_prop_type(Rsed.core.project().track_id(),
-                                                                 Rsed.ui_input_n.mouse_hover_args().trackId,
-                                                                 Rsed.core.project().props.id_for_name(name));
+                                                               Rsed.ui_input_n.mouse_hover_args().trackId,
+                                                               Rsed.core.project().props.id_for_name(name));
                     window.close_dropdowns();
 
                     return;
                 },
                 
-                refresh:function()
+                refresh: function()
                 {
                     this.trackName = Rsed.core.project().name;
                     this.propList = Rsed.core.project().props.names()

js/rallysported/track/props.js
@@ -137,6 +137,15 @@ Rsed.track.props = async function(textureAtlas = Uint8Array)
                             (textureId < propTextures.length))
                         || Rsed.throw("Attempting to access prop textures out of bounds.");
 
+            args =
+            {
+                ...
+                {
+                    alpha: true,
+                },
+                ...args,
+            };
+
             return Object.freeze({...propTextures[textureId], alpha:args.alpha});
         },
 