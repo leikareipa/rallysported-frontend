9314e0d4e341b08395a5f56875c623097429a7e5
leikareipa <drilledicecream@gmail.com>
2020-10-09 16:08:58 +0300

    Fix logic error in Rsed.throw_if()

--- src/client/js/rallysported-js/rallysported.js
+++ src/client/js/rallysported-js/rallysported.js
@@ -13,12 +13,14 @@ const Rsed = {};
 
 // Various small utility functions and the like.
 {
-    // Defined 'true' to allow for the conveniency of named in-place assertions,
-    // e.g. Rsed.assert && (x === 1) || Rsed.throw("X wasn't 1.").
+    // For inline assertions, e.g.:
+    //
+    //   Rsed.assert && (x === 1)
+    //               || Rsed.throw("X wasn't 1.").
+    //
     // Note that setting this to 'false' won't disable assertions - for that,
     // you'll want to search/replace "Rsed.assert &&" with "Rsed.assert ||"
-    // and keep this set to 'true'. The comparison against Rsed.assert may still
-    // be done, though (I guess depending on the JS engine's ability to optimize).
+    // and keep this set to 'true'.
     Object.defineProperty(Rsed, "assert", {value:true, writable:false});
 
     // Generates a version 4 UUID and returns it as a string. Adapted with
@@ -49,11 +51,11 @@ const Rsed = {};
         console.log("RallySportED: " + message);
     }
 
-    Rsed.throw_if = (condition, messageIfFalse)=>
+    Rsed.throw_if = (condition, messageIfTrue)=>
     {
-        if (!condition)
+        if (condition)
         {
-            Rsed.throw(messageIfFalse)
+            Rsed.throw(messageIfTrue)
         }
     }
 
